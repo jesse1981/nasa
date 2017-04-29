6e3a508f18a9d5640dcdac4e6b3bdf3f810911ef
Jesse Bryant <jesse.bryant@gmail.com>
2017-04-29 11:23:56 +1000

    Started on web frontend scaffold

---
+++ www/templates/scripts.php
@@ -0,0 +1,17 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="../js/mustache.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+    $('div[data-template]').each(function(){
+        $.when($.ajax({url: $(this).attr("data-template"), dataType: 'text'}),$.ajax({url: $(this).attr("data-model")}))
+	.done(function(template, data){
+            Mustache.parse(template[0]);
+            var rendered = Mustache.render(template[0], {panels: data[0].panels});
+            $(this).html(rendered);
+	})
+	.fail(function(){
+            alert("Sorry there was an error on "+$(this).attr("name"));
+	});
+    });
+});
+</script>
