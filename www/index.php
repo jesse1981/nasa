b3a4828e88d0c39da62ac6eda0c4a3132adfb520
Jesse Bryant <jesse.bryant@gmail.com>
2017-04-29 11:25:00 +1000

    Started on web frontend scaffold

---
+++ www/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include 'templates/head.php'; ?>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+    <?php include 'templates/scripts.php' ?>
+</html>
